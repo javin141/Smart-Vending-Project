31f6e241d9055012213916e0953b14b6b955c1c3
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-07-10 00:13:16 +0800

    Utility function to beep when there are exceptions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
+from hal import hal_buzzer
 def serialise_ints(ints: list[int]) -> str:
     return ",".join(str(x) for x in ints)
 
 def deserialise_ints(ser: str) -> list[int]:
     return [int(x) for x in ser.split(",")]
+
+
+def alert_when_exception():
+    hal_buzzer.init()
+    hal_buzzer.beep(1, 1, 4)
