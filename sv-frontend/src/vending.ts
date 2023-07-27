a0b522e96a81676a9a432dac681433e7f258f8f8
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-07-27 15:52:32 +0800

    payFor in frontend (POST)

--- sv-frontend/src/vending.ts
+++ sv-frontend/src/vending.ts
@@ -1,11 +1,13 @@
-import {setCookie} from "./utils";
-import {LoginResult} from "./auth";
-import {VendingItem} from "./objs/VendingItem";
 
+import {VendingItem} from "./objs/VendingItem";
+import {CardDetails, getBearer} from "./auth.ts";
+import {errPhraseToMsg, ErrResult} from "./objs/ErrResult.ts";
+
+const ROOT_BACKEND = "http://localhost:6788/"
 export async function getItems(): Promise<VendingItem[]|null> {
 
 
-    const result = await fetch("http://localhost:6788/drinks", {
+    const result = await fetch(`${ROOT_BACKEND}/drinks`, {
         method: "GET",
         // mode: "cors", TODO get CORS working properly when in production
         cache: "no-cache",
@@ -13,7 +15,7 @@ export async function getItems(): Promise<VendingItem[]|null> {
     })
 
     if (result.ok) {
-        const vendingItems = (await result.json())
+        const vendingItems = (await result.json()) as VendingItem[]
         return vendingItems
     } else {
         return null
@@ -21,3 +23,46 @@ export async function getItems(): Promise<VendingItem[]|null> {
 
 
 }
+
+interface RedemtionDetails {
+    redeemCode: string,
+    time: number
+}
+
+
+
+export async function payFor(item: VendingItem, cardDetails: CardDetails): Promise<RedemtionDetails> {
+    const url = `${ROOT_BACKEND}/drinks`
+    const body = {
+        refcode: item.refcode,
+        paymentDetails: cardDetails
+    }
+    const bearer = getBearer()
+
+    const result = await fetch(url, {
+        method: "POST",
+        cache: "no-cache",
+        body: JSON.stringify(body),
+        headers: {Authorization: bearer}
+    })
+
+    if (result.ok) {
+        const redemtionDetails = await result.json() as RedemtionDetails
+        return redemtionDetails
+    } else {
+        const err = await result.json() as ErrResult
+        const errHrMsg = errPhraseToMsg(err.errphrase, err.error || "Error!")
+        throw new Error(errHrMsg)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
