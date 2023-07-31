571ec3ec558d7277d2120bb094cd9d88dd5918cb
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-07-31 14:51:04 +0800

    Order interface and relevant functions

---
+++ sv-frontend/src/orders.ts
@@ -0,0 +1,42 @@
+import {getBearer} from "./auth.ts";
+import {errPhraseToMsg, ErrResult} from "./objs/ErrResult.ts";
+import {ROOT_BACKEND} from "./utils.ts";
+
+export interface Order {
+    refcode: number,
+    name: string,
+    price: number,
+    redeemCode: string,
+    time: number,
+    slot: number,
+    user: string,
+    id: string,
+    _id: string
+}
+
+
+async function getOrders(): Promise<Order[]> {
+    const bearer = getBearer()
+    const result = await fetch(`${ROOT_BACKEND}/drinks/orders`, {
+        method: "GET",
+        cache: "no-cache",
+        headers: {
+            Authorization: bearer
+        }
+    })
+    console.log("result,", result)
+    if (result.ok) {
+        console.log("orders")
+        const orders = await result.json() as Order[]
+        console.log(orders)
+        // NOTE: page will CRASH if server sends malformatted contents!
+        return orders
+    } else {
+        const err = await result.json() as ErrResult
+        const errHrMsg = errPhraseToMsg(err.errphrase, err.error || "Error!")
+        console.log(err, errHrMsg)
+        throw new Error(errHrMsg)
+    }
+}
+
+export {getOrders}
