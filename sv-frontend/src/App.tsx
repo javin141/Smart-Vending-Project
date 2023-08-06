491b518ade621b12f648cee8bae5c9963daef962
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-08-06 21:32:35 +0800

    Clear App.tsx

--- sv-frontend/src/App.tsx
+++ sv-frontend/src/App.tsx
@@ -1,19 +1,12 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import MyAppBar from "./components/MyAppBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const dispatch = useDispatch()
+    return (
+        <div style={{display: "flex", flexDirection: "column"}}>
 
-  return (
-      <div style={{display: "flex", flexDirection: "column"}}>
-
-      </div>
-  )
+        </div>
+    )
 }
 
 export default App
