f893bd38dcf830465f15c1b74d143b5bec14bc7f
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-08-06 23:51:37 +0800

    Edits to launch entry point

--- src/launch.py
+++ src/launch.py
@@ -1,4 +1,5 @@
-import argparse, selection
+import argparse
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--launch", type=str)
@@ -6,8 +7,18 @@ parser.add_argument("-l", "--launch", type=str)
 args = parser.parse_args()
 
 launch = args.launch
+print("Launched with", launch)
 
 if launch == "selection":
+    import selection, antitheft_background
+    antitheft_background.launch()
     selection.main()
+elif launch == "online":
+    import online, antitheft_background
+    antitheft_background.launch()
+    online.main()
+elif launch == "burglar":
+    import breakin
+    breakin.main()
 else:
     print("Invalid choice!")
