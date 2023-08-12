b5fea10d2fa46db9bd6f03ebdf31329f5f7c6776
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-08-12 18:24:03 +0800

    Removed preview + format data

--- src/camera.py
+++ src/camera.py
@@ -5,12 +5,9 @@ import time
 
 picam2 = Picamera2()
 
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-picam2.configure(preview_config)
 
 
 def capture_to(file_path: str):
-    picam2.start_preview(Preview.QTGL)
 
     picam2.start()
     time.sleep(1)
@@ -26,6 +23,8 @@ def read_barcode_loop_sync() -> str:
     font = cv2.FONT_HERSHEY_SIMPLEX
     scale = 1
     thickness = 2
+    preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+    picam2.configure(preview_config)
 
     def draw_barcodes(request):
         with MappedArray(request, "main") as m:
@@ -35,16 +34,25 @@ def read_barcode_loop_sync() -> str:
                     y = min([p.y for p in b.polygon]) - 30
                     cv2.putText(m.array, b.data.decode('utf-8'), (x, y), font, scale, colour, thickness)
 
-    picam2 = Picamera2()
-    picam2.start_preview(Preview.QTGL)
-    config = picam2.create_preview_configuration(main={"size": (1280, 960)})
-    picam2.configure(config)
+
 
     barcodes = []
     picam2.post_callback = draw_barcodes
+    picam2.start_preview(Preview.QTGL)
     picam2.start()
     while True:
         rgb = picam2.capture_array("main")
         barcodes = decode(rgb)
         if len(barcodes) == 1:
-            return barcodes[0]
+            return barcodes[0].data.decode("utf-8")
+
+
+# For debug
+if __name__ == "__main__":
+    while True:
+        try:
+            print(eval(input(">>> ")))
+        except KeyboardInterrupt:
+            break
+        except:
+            continue
