10920ddcd8fd4b4de94b2b1df7178602959579ad
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-08-13 17:23:30 +0800

    Refresh every min

--- sv-frontend/src/objs/Order.tsx
+++ sv-frontend/src/objs/Order.tsx
@@ -10,14 +10,19 @@ interface OrderProp {
 
 const HOURS_24 = 86400
 
+
+function secToHHMM(sec: number): string {
+    const mins = Math.trunc(sec / 60)
+    const hours = Math.trunc(mins / 60)
+    const minsDisp = (mins % 60).toString().padStart(2, "0")
+    return `${hours}:${minsDisp}`
+}
 function reservationExpiringMsg(order: Order) {
     const timeNow = getUnixTime()
-    const date = new Date();
     const diff = 86400 - (timeNow - order.time)
 
-    date.setSeconds(diff);
     if (diff < 0) return "Order is in the future"
-    else if (0 < diff && diff < HOURS_24) return `Reservation expiring in ${date.toISOString().slice(11, 19)}`
+    else if (0 < diff && diff < HOURS_24) return `Reservation expiring in ${secToHHMM(diff)}`
     else return "Reservation has expired. Subject to stock availability at machine."
 }
 
@@ -29,10 +34,11 @@ const OrderCard = ({order, onClick}: OrderProp) => {
         setTimeout(() => {
             setExpiringMsg(reservationExpiringMsg(order))
             refreshTime()
-        }, 1000)
+        }, 60000)
     }
 
     useEffect(() => {
+        setExpiringMsg(reservationExpiringMsg(order))
         refreshTime()
     })
 
