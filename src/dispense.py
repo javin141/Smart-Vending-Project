7196cc404b2c9bff73340304c4642c18cd2ef19f
Lucius C. (czlucius) <58442255+czlucius@users.noreply.github.com>
2023-08-14 15:15:50 +0800

    Dispense bugfixes

--- src/dispense.py
+++ src/dispense.py
@@ -3,9 +3,11 @@ import time
 from Inventory_Array import get_item
 from hal.hal_lcd import lcd
 from hal.hal_servo import set_servo_position, init
-from hal.hal_led import init, set_output
+from hal.hal_led import set_output
+from hal import hal_led, hal_servo
 from hal.hal_buzzer import init, turn_on_with_timer
 
+import RPi.GPIO as GPIO
 
 def dispense_drink(refcode):
 
@@ -14,7 +16,9 @@ def dispense_drink(refcode):
 
     if item:
         # Initialize the servo and LCD
-        init()
+        hal_servo.init()
+        hal_led.init()
+
         lcd_dispenser = lcd()
 
         # Spin the servo 180 degrees slowly
@@ -29,13 +33,13 @@ def dispense_drink(refcode):
         for i in range(0, 180, 5):
             set_servo_position(i)
 
-        # Wait for 10 seconds
-        time.sleep(3.5)
         turn_on_with_timer(0.5)
 
         # Spin the servo back to 0 degrees
         for i in range(180, 0, -5):
+            set_output(24, GPIO.HIGH)
             set_servo_position(i)
+            set_output(24, GPIO.LOW)
         
         set_output(24, GPIO.LOW)
 
@@ -44,13 +48,8 @@ def dispense_drink(refcode):
         lcd_dispenser.lcd_display_string("Dispensing", 1)
         lcd_dispenser.lcd_display_string("Complete!", 2)
 
-        # Wait for another 10 seconds
-        time.sleep(4)
-        for i in range(0, 10, 1):
-            set_output(24, GPIO.HIGH)
-            turn_on_with_timer(0.5)
-            set_output(24, GPIO.LOW)
-            time.sleep(0.5)
+        time.sleep(3)
+
 
         # Clear the display
         lcd_dispenser.lcd_clear()
